03acfdac681aaa5b9cdb4ea678ad5d8aba96069e
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-28 11:18:07 +0100

    Index file added

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+var server = app.listen(8080, function(){
+  console.log('Magic is happening on port 8080');
+});
+
+
+
+
