785df08280cde8b6567f6c2919a61774d657ddb6
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-29 19:10:56 +0100

    private token set

--- commit.sh
+++ commit.sh
@@ -9,6 +9,5 @@
  \"subscribed\" : true,
  \"title\": \"${GITLAB_USER_NAME} merge request for: ${CI_COMMIT_REF_SLUG}\" }"; 
  
-RESULT=$(curl -X POST "${CI_API_V4_URL}${CI_PROJECT_ID}/merge_requests" -H "PRIVATE-TOKEN:${PRIVATE_TOKEN}" -H "Content-Type: application/json" -D "${BODY}");
-eval $RESULT; 
+RESULT="$(curl -X POST "${CI_API_V4_URL}${CI_PROJECT_ID}/merge_requests" -H "PRIVATE-TOKEN:${PRIVATE_TOKEN}" -H "Content-Type: application/json" --data "${BODY}")";
  echo $RESULT;
