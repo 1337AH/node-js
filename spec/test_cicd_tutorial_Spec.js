a7b966563c123d00b90b5c944ef31d9c1f43b288
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-29 12:29:15 +0100

    fixed release

--- spec/test_cicd_tutorial_Spec.js
+++ spec/test_cicd_tutorial_Spec.js
@@ -6,9 +6,18 @@ describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(404);
         done();
       });
     });
+
+    it("returns Hello World", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toBe("Hello World");
+        helloWorld.closeServer();
+        done();
+      });
+    });
+  });
 });
-});
+
