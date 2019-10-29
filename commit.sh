a4566bdff9f2bcd0f1cc3e0236fe376331d61490
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-29 17:09:31 +0100

    changed

--- commit.sh
+++ commit.sh
@@ -1,42 +1,13 @@
-#!/bin/sh
-# TODO determine URL from git repository URL
-[[ $HOST =~ ^https?://[!/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
+ BODY="{ \"project_id\": ${CI_PROJECT_ID},
+ \"source_branch\": \"${CI_COMMIT_REF_NAME}\",
+ \"target_branch\": \"${TARGET_BRANCH}\",
+ \"remove_source_branch\": false,
+ \"force_remove_source_branch\": false,
+ \"allow_collaboration\": true,
+ \"subscribed\" : true,
+ \"title\": \"${GITLAB_USER_NAME} merge request for: ${CI_COMMIT_REF_SLUG}\" }"; 
 
-# The branch which we wish to merge into
-TARGET_BRANCH=staging;
+ RESULT=$('curl -X POST "${CI_API_V4_URL}${CI_PROJECT_ID}/merge_requests" -H "PRIVATE-TOKEN:${PRIVATE_TOKEN}" -H "Content-Type: application/json" -D "${BODY}"'); 
+ echo $RESULT;
 
-# The user's token name so that we can open the merge request as the user
-TOKEN_NAME="$(echo "${GITLAB_USER_LOGIN}"_COMMIT_TOKEN | tr '[:lower:]' '[:upper:]')"
 
-# See: http://www.tldp.org/LDP/abs/html/parameter-substitution.html search ${!varprefix*}, ${!varprefix@} section
-PRIVATE_TOKEN="${!TOKEN_NAME}"
-
-# The description of our new MR, we want to remove the branch after the MR has
-# been closed
-BODY="{
-\"project_id\": ${CI_PROJECT_ID},
-\"source_branch\": \"${CI_COMMIT_REF_NAME}\",
-\"target_branch\": \"${TARGET_BRANCH}\",
-\"remove_source_branch\": false,
-\"force_remove_source_branch\": false,
-\"allow_collaboration\": true,
-\"subscribed\" : true,
-\"title\": \"${GITLAB_USER_NAME} merge request for: ${CI_COMMIT_REF_SLUG}\"
-}";
-
-# Require a list of all the merge request and take a look if there is already
-# one with the same source branch
- LISTMR=$(curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}");
- COUNTBRANCHES=$(echo "${LISTMR}" | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | c -l);
-
-# No MR found, let's create a new one
-if [ "${COUNTBRANCHES}" -eq "0" ]; then
-    curl -X POST "${HOST}${CI_PROJECT_ID}/merge_requests" \
-    --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" \
-    --header "Content-Type: application/json" \
-    --data "${BODY}";
-
-    echo "Opened a new merge request: WIP: ${CI_COMMIT_REF_SLUG} for user ${GITLAB_USER_LOGIN}";
-    exit;
-fi
-    echo "No new merge request opened"
