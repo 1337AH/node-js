1eae2f19b6ed93a94ff114289a71e2f6ed8826d0
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-30 10:17:27 +0100

    fix9

--- commit.sh
+++ commit.sh
@@ -4,9 +4,9 @@ TARGET_BRANCH=staging;
 
 BODY='{"id": '${CI_PROJECT_ID}', "source_branch": '${CI_COMMIT_REF_NAME}', "target_branch": '${TARGET_BRANCH}', "remove_source_branch": false, "force_remove_source_branch": false, "allow_collaboration": true, "subscribed" : true, "title": '${GITLAB_USER_NAME}', merge request for: '${CI_COMMIT_REF_SLUG}' }'; 
 
-RESULT= $('curl -X POST "'${CI_API_V4_URL}'/'${CI_PROJECT_ID}'/merge_requests" -H "PRIVATE-TOKEN:'${GITLAB_PRIVATE_TOKEN}'" -H "Content-Type: application/json" -d "'${BODY}'"');
+RESULT= $('curl --request POST "'${CI_API_V4_URL}'/'${CI_PROJECT_ID}'/merge_requests" --header "PRIVATE-TOKEN:'${PRIVATE_TOKEN}'" --header "Content-Type: application/json" --data "'${BODY}'"');
 
-echo $RESULT;
+echo $RESULT;	
 
 
 
