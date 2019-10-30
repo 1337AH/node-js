5e7d2c5040f95b780ed6eacd2e0882798f896cb5
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-30 17:24:16 +0100

    dsd

--- commit.sh
+++ commit.sh
@@ -1,43 +1,7 @@
-#!/bin/sh
+RESULT=$(curl -d '{"id":15033822, "source_branch":"dev", "target_branch":"staging", "title":"Build and Tests has been done successfully and moved to staging","squash":"false" }' -H "Content-Type: application/json" -H "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" -X POST "http://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests");
 
-if [ -z "$GITLAB_PRIVATE_TOKEN" ]; then
-  echo "GITLAB_PRIVATE_TOKEN not set"
-  echo "Please set the GitLab Private Token as GITLAB_PRIVATE_TOKEN"
-  exit 1
-fi
-
-# Extract the host where the server is running, and add the URL to the APIs
-[[ $HOST =~ ^https?://[^/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
-
-# Look which is the default branch
-TARGET_BRANCH="staging";
-
-# The description of our new MR, we want to remove the branch after the MR has
-# been closed
-BODY="{
-    \"id\": ${CI_PROJECT_ID},
-    \"source_branch\": \"${CI_COMMIT_REF_NAME}\",
-    \"target_branch\": \"${TARGET_BRANCH}\",
-    \"remove_source_branch\": true,
-    \"title\": \"WIP: ${CI_COMMIT_REF_NAME}\",
-    \"assignee_id\":\"${GITLAB_USER_ID}\"
-}";
-
-# Require a list of all the merge request and take a look if there is already
-# one with the same source branch
-LISTMR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}"`;
-COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
-
-# No MR found, let's create a new one
-if [ ${COUNTBRANCHES} -eq "0" ]; then
-    curl -X POST "${HOST}${CI_PROJECT_ID}/merge_requests" \
-        --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}" \
-        --header "Content-Type: application/json" \
-        --data "${BODY}";
-
-    echo "Opened a new merge request: WIP: ${CI_COMMIT_REF_NAME} and assigned to you";
-    exit;
-fi
-
-echo "No new merge request opened";
+eval $RESULT;
+echo ${CI_PROJECT_ID};
+echo ${PRIVATE_TOKEN};
+echo $RESULT;
 
