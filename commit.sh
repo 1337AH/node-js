0529b11d9000b34e69286d5678e1a315f511c609
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-30 14:14:12 +0100

    not ending

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 RESULT=$(curl -d '{ "id":15033822, "source_branch":"dev", "target_branch":"staging", "title":"Build and Tests has been done successfully and moved to staging","squash":"false" }' -H "Content-Type: application/json" -H "Private-Token: ${PRIVATE_TOKEN}" -X POST "http://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests")
 
