fc368ec51da8dd0c93bde03b96bb831d4f8e3b84
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-31 11:40:37 +0100

    url ch

--- commit.sh
+++ commit.sh
@@ -1,6 +1,5 @@
-RESULT=$(curl -d '{"id":15033822, "source_branch":"dev", "target_branch":"staging", "title":"Build and Tests has been done successfully and moved to staging","squash":"false" }' -H "Content-Type: application/json" -H "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" -X POST "http://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests");
+RESULT=$(curl -d '{"id":15033822, "source_branch":"dev", "target_branch":"staging", "title":"Build and Tests has been done successfully and moved to staging","squash":"false" }' -H "Content-Type: application/json" -H "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" -X POST "http://gitlab.com/arlindh/api/v4/projects/${CI_PROJECT_ID}/merge_requests");
 
-eval $RESULT;
 echo ${CI_PROJECT_ID};
 echo ${GITLAB_PRIVATE_TOKEN};
 echo $RESULT;
