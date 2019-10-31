81093d521a86eb3ed4df877b93b91986cf21b570
Arlind Hoxha <ahoxha.01@gmail.com>
2019-10-31 17:16:39 +0100

    fail to hotfix

--- spec/test_cicd_tutorial_Spec.js
+++ spec/test_cicd_tutorial_Spec.js
@@ -6,7 +6,7 @@ describe("Hello World Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
-        eexpect(response.statusCode).toBe(404);
+        expect(response.statusCode).toBe(404);
         done();
       });
     });
